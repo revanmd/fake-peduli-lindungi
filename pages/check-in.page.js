3f8adc754058a077f1e2dca4236ba173182ee828
revanmd <revanmdafa@gmail.com>
2022-07-31 01:59:12 +0700

    test mobile ui

--- pages/check-in.page.js
+++ pages/check-in.page.js
@@ -5,21 +5,46 @@ const CheckIn = () => {
             width:'100vw',
             backgroundColor:'#eeeeee'
         }}>
-            <div>Close Button</div>
             <div
                 style={{
-                    width:'150px',
-                    marginLeft:'auto',
-                    marginRight:'auto',
-                    textAlign:'center'
+                    position:'relative',
+                    top:'10px',
+                    marginLeft:'10px'
                 }}
             >
-                Check-In Gagal
+                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="#074973" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+            </div>
+            <div
+                style={{
+                    textAlign:'center',
+                    marginTop:'35%'
+                }}
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#107e42" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
+            </div>
+            <div
+                style={{
+                    width:'200px',
+                    marginLeft:'auto',
+                    marginRight:'auto',
+                    textAlign:'center',
+                    marginTop:'5px'
+                }}
+            >
+                <span
+                    style={{
+                        fontSize:'16px', 
+                        color:'#0c7f41',
+                        fontWeight:600
+                    }}
+                >
+                    Check-In Berhasil
+                </span>
             </div>
             <div
                 style={{
                     padding:'30px',
-                    marginTop:'50%'
+                    marginTop:'5px'
                 }}
             >
                 <div
@@ -27,14 +52,14 @@ const CheckIn = () => {
                         backgroundColor:'#FFFFFF',
                         width:'100%',
                         borderRadius:'15px',
-                        padding:'15px 40px',
+                        padding:'15px 25px',
                         boxShadow: 'rgba(0, 0, 0, 0.3) 0px 2px 10px'
                     }}
                 >
                     <button
                         style={{
                             width:'130px',
-                            padding:'3px',
+                            padding:'2px',
                             borderRadius:'10px',
                             border:'solid 1px black',
                             backgroundColor:'#FFFFFF',
@@ -51,18 +76,25 @@ const CheckIn = () => {
                         textAlign:'center',
                         margin:'10px 0'
                     }}>
+                        <svg 
+                            style={{
+                                position:'relative',
+                                top:'4px',
+                                marginRight:'5px'
+                            }}
+                            xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#080808" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" classNameName="feather feather-map-pin"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>
                         <span
                             style={{
                                 fontSize:'18px',
                                 fontWeight:'bold',
                             }}
                         >
-                            Mall Lippo Cikarang
+                            Puri Mansion Apartement
                         </span>
                     </div>
                     <div style={{
                         textAlign:'center',
-                        margin:'10px 0'
+                        margin:'5px 0'
                     }}>
                         <span
                             style={{
@@ -94,7 +126,7 @@ const CheckIn = () => {
                         style={{
                             width:'100%',
                             fontSize:'13px',
-                            fontWeight:600
+                            fontWeight:800
                         }}
                     >
                         <div
@@ -103,7 +135,14 @@ const CheckIn = () => {
                                 display:'inline-block',
                                 textAlign:'center'
                             }}
-                        >
+                        >   
+                            <svg 
+                                style={{
+                                    position:'relative',
+                                    top:'4px',
+                                    marginRight:'3px'
+                                }}
+                                xmlns="http://www.w3.org/2000/svg" width="15" height="20" viewBox="0 0 24 24" fill="none" stroke="#080808" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
                             <span>
                                 24 Jul 2022
                             </span>
@@ -115,6 +154,13 @@ const CheckIn = () => {
                                 textAlign:'center'
                             }}
                         >
+                            <svg 
+                                style={{
+                                    position:'relative',
+                                    top:'4px',
+                                    marginRight:'3px'
+                                }}
+                                xmlns="http://www.w3.org/2000/svg" width="15" height="20" viewBox="0 0 24 24" fill="none" stroke="#080808" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-clock"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>
                             <span>
                                 07:14 PM
                             </span>
@@ -122,7 +168,7 @@ const CheckIn = () => {
                     </div>
                     <div
                         style={{
-                            marginTop:'10px'
+                            marginTop:'20px'
                         }}
                     >
                         <button
@@ -134,7 +180,8 @@ const CheckIn = () => {
                                 letterSpacing:0,
                                 border:"none",
                                 borderRadius:'20px',
-                                backgroundColor:'#ffc044'
+                                backgroundColor:'#047a40',
+                                color:'white'
                             }}
                         >
                             Revan Muhammad Dafa
